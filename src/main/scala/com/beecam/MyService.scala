015ddd1aca87b4eb4b74ed554de295221b4a0526
unknown <mcherri@users.noreply.github.com>
2015-06-02 23:58:10 +0200

    Add index page.

--- src/main/scala/com/beecam/MyService.scala
+++ src/main/scala/com/beecam/MyService.scala
@@ -48,7 +48,7 @@ trait MyService extends HttpService {
           complete {
             <html>
               <body>
-                <h1>Say hello to <i>spray-routing</i> on <i>Jetty</i>!</h1>
+                <h1>Say hello to <i>spray-routing</i>!</h1>
               </body>
             </html>
           }
